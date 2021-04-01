af5fc595bfa9681deb3cc76391ec2b9f844b10ee
Git Server Admin <admin@git-server>
2021-04-01 11:37:13 +0000

    Add initial repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Form from './components/Form'
+import Message from './components/Message'
+
+function App() {
+  return (
+    <div>
+      <Form></Form>
+      <Message></Message>
+    </div>
+  )
+}
+
+export default App
